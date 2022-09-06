305b8d11c3b26e5211997b37f2775d033f5a4748
Nutty <nuttanai@livingmobile.me>
2022-09-06 17:02:59 +0700

    init

---
+++ Finomena/Finomena/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finomena
+//
+//  Created by Nuttanai on 6/9/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
